6f1935a2e81d12974f9c42f689f2f9436036dbd9
PalvarezLara <pame.alvarez@duocuc.cl>
2025-10-25 01:11:47 -0300

    🛠️ chore: configuración de Netlify para despliegue SPA (redirects + build)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/mock': {
+        target: 'https://demo6694663.mockable.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/mock/, ''),
       },
-    }),
-  ],
+    },
+  },
 })
